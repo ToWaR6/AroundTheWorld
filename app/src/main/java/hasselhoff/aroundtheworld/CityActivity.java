6f72a8d656009cbe19728dee76617197ae7cfec1
Licinius <florent.dellomo@gmail.com>
2018-03-12 16:20:01 +0100

    City Activity with Layout

---
+++ app/src/main/java/hasselhoff/aroundtheworld/CityActivity.java
@@ -0,0 +1,50 @@
+package hasselhoff.aroundtheworld;
+
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+public class CityActivity extends SubActivity implements OnConnectionFailedListener{
+    private GoogleApiClient myGoogleApiClient;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        myGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i("city","La ville est : " + place);
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(null, "An error occurred: " + status);
+            }
+        });
+    }
+    public void onConnectionFailed (ConnectionResult result){
+        //TO DO
+    }
+}
--- app/src/main/java/hasselhoff/aroundtheworld/MainActivity.java
+++ app/src/main/java/hasselhoff/aroundtheworld/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 
@@ -49,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void openAdsAct(View view) {
         newActivity(AdsActivity.class);
+    }
+
+    public void openMyCity (MenuItem menuItem){
+        newActivity(CityActivity.class);
     }
 
 }
