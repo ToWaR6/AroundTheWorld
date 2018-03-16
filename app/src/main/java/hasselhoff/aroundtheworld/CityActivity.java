e7adcf248495fa3eda992177094e2ca8ea847524
Licinius <florent.dellomo@gmail.com>
2018-03-16 16:21:56 +0100

    Suppression API KEY

--- app/src/main/java/hasselhoff/aroundtheworld/CityActivity.java
+++ app/src/main/java/hasselhoff/aroundtheworld/CityActivity.java
@@ -1,9 +1,12 @@
 package hasselhoff.aroundtheworld;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -16,7 +19,6 @@ import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 public class CityActivity extends SubActivity implements OnConnectionFailedListener{
     private GoogleApiClient myGoogleApiClient;
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
@@ -45,6 +47,9 @@ public class CityActivity extends SubActivity implements OnConnectionFailedListe
         });
     }
     public void onConnectionFailed (ConnectionResult result){
-        //TO DO
+       Log.d("PROBLEM","ça marche pas là");
+    }
+    public void close(View view){
+        CityActivity.this.finish();
     }
 }
