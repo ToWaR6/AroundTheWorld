d7ff677b60e938d7c163c7913d901b7963f72184
Licinius <florent.dellomo@gmail.com>
2018-04-16 10:29:36 +0200

    Connect API Ads  :  - Like button  - dislike button  - without opinion button  - open browser

--- app/src/main/java/hasselhoff/aroundtheworld/AdsActivity.java
+++ app/src/main/java/hasselhoff/aroundtheworld/AdsActivity.java
@@ -1,37 +1,52 @@
 package hasselhoff.aroundtheworld;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
+
 import hasselhoff.aroundtheworld.database.DatabaseHandler;
 import hasselhoff.aroundtheworld.database.PersonContract;
 import hasselhoff.aroundtheworld.remote_fetch.RemoteFetchAd;
+import hasselhoff.aroundtheworld.remote_fetch.DownloadImageTask;
 
 public class AdsActivity extends SubActivity {
     private Handler handler;
     private String currentCity;
+    private String adUrl;
     private ImageView adImage;
+    private TextView adTitle;
+    private Button likeButton;
+    private Button dislikeButton;
+    private Button withoutOpinionButton;
+    private ArrayList<String> adId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         handler = new Handler();
+        adId = new ArrayList<>();
         setContentView(R.layout.activity_ads);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        likeButton = findViewById(R.id.likeButton);
+        dislikeButton = findViewById(R.id.dislikeButton);
+        withoutOpinionButton = findViewById(R.id.withoutOpinionButton);
         adImage = findViewById(R.id.adImage);
+        adTitle = findViewById(R.id.adTitle);
 
         DatabaseHandler myDatabaseHandler = new DatabaseHandler(AdsActivity.this);
         SQLiteDatabase db = myDatabaseHandler.getReadableDatabase();
@@ -63,6 +78,7 @@ public class AdsActivity extends SubActivity {
                         @Override
                         public void run() {
                             Toast.makeText(AdsActivity.this, AdsActivity.this.getString(R.string.ad_not_found), Toast.LENGTH_LONG).show();
+                            renderOriginAd();
                         }
                     });
                 }else{
@@ -75,18 +91,107 @@ public class AdsActivity extends SubActivity {
             }
         }.start();
     }
+    public void getOnlyNewAd(final String city){
+        new Thread(){
+            public void run(){
+                final JSONObject json = RemoteFetchAd.postJSON(AdsActivity.this,city,adId.toArray());
+                if(json == null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(AdsActivity.this, AdsActivity.this.getString(R.string.ad_not_found), Toast.LENGTH_LONG).show();
+                            renderOriginAd();
+                        }
+                    });
+                }else{
+                    handler.post(new Runnable(){
+                        public void run(){
+                            renderAd(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    public void likeAd(final String _id){
+        new Thread(){
+            public void run(){
+                final JSONObject json = RemoteFetchAd.likeAd(AdsActivity.this,_id);
+                if(json == null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(AdsActivity.this, AdsActivity.this.getString(R.string.ad_not_found), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }else{
+                    handler.post(new Runnable(){
+                        public void run(){
+                            Toast.makeText(AdsActivity.this, AdsActivity.this.getString(R.string.feedback), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+    public void dislikeAd(final String _id){
+        new Thread(){
+            public void run(){
+                final JSONObject json = RemoteFetchAd.dislikeAd(AdsActivity.this,_id);
+                if(json == null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(AdsActivity.this, AdsActivity.this.getString(R.string.ad_not_found), Toast.LENGTH_LONG).show();
+                            renderOriginAd();
+                        }
+                    });
+                }else{
+                    handler.post(new Runnable(){
+                        public void run(){
+                            Toast.makeText(AdsActivity.this, AdsActivity.this.getString(R.string.feedback), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
     public void renderAd(JSONObject json){
         try{
-            Log.i("Test",json.getString("imageURL"));
-            URL url = new URL(json.getString("imageURL"));
-            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-            adImage.setImageBitmap(bmp);
+            adUrl = json.getString("advertisingURL");
+            adId.add(json.getString("_id")); //For like and dislike button
+            new DownloadImageTask(adImage).execute(json.getString("imageURL"));
+            adTitle.setText(json.getString("name"));
         } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    public void renderOriginAd(){
+        adImage.setImageDrawable(ContextCompat.getDrawable(AdsActivity.this,R.drawable.pub));
+        adTitle.setText(R.string.ad);
+        dislikeButton.setEnabled(false);
+        likeButton.setEnabled(false);
+        withoutOpinionButton.setEnabled(false);
+        adUrl = null;
+    }
+    public void withoutOpinion(View view){
+        getOnlyNewAd(currentCity);
+    }
+    public void like(View view){
+        likeAd(adId.get(adId.size() -1 ));
+        getOnlyNewAd(currentCity);
+    }
+    public void dislike(View view){
+        dislikeAd(adId.get(adId.size() -1 ));
+        getOnlyNewAd(currentCity);
+    }
+
+    public void openBrowser(View view){
+        if(adUrl != null) {
+            Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(adUrl));
+            startActivity(intent);
+        }
+    }
+
 }
--- app/src/main/java/hasselhoff/aroundtheworld/remote_fetch/RemoteFetchAd.java
+++ app/src/main/java/hasselhoff/aroundtheworld/remote_fetch/RemoteFetchAd.java
@@ -1,23 +1,25 @@
 package hasselhoff.aroundtheworld.remote_fetch;
 
 import android.content.Context;
-import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import hasselhoff.aroundtheworld.R;
 
-/**
- * Created by Marvin on 15/04/2018.
- */
+
 
 public class RemoteFetchAd {
     private static final String AD_API =
@@ -26,8 +28,11 @@ public class RemoteFetchAd {
     public static JSONObject getJSON(Context context, String city){
         try{
             String urlString = AD_API + "/ads/city/%s";
-            URL url = new URL(String.format(urlString, city)); //Ajout de la ville dans l'url
+            city = URLEncoder.encode(city,"utf-8");
+            urlString = String.format(urlString, city);
+            URL url = new URL(urlString); //Ajout de la ville dans l'url
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             StringBuffer json = new StringBuffer(1024);
             String tmp;
@@ -35,8 +40,104 @@ public class RemoteFetchAd {
                 json.append(tmp).append("\n");
             reader.close();
             JSONObject data = new JSONObject(json.toString());
-            if(data.getInt("cod") != 200)// != Réussite
-                return null;
+            /*if(data.getInt("cod") != 200)// != Réussite
+                return null;*/
+            return data;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static JSONObject postJSON(Context context, String city,Object[] _ids) {
+        try {
+            String urlString = AD_API + "/ads/city/%s";
+            city = URLEncoder.encode(city, "utf-8");
+            urlString = String.format(urlString, city);
+            URL url = new URL(urlString); //Ajout de la ville dans l'url
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            OutputStream out = new BufferedOutputStream(connection.getOutputStream());
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+
+            writer.write("{_id : [");
+            for (int index = 0; index < _ids.length; index++) {
+                writer.write((String)_ids[index]);
+                if (index != _ids.length - 1)
+                    writer.write(",");
+            }
+            writer.write("]}");
+            writer.flush();
+            writer.close();
+            out.close();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer json = new StringBuffer(1024);
+            String tmp;
+            while ((tmp = reader.readLine()) != null)
+                json.append(tmp).append("\n");
+            reader.close();
+            JSONObject data = new JSONObject(json.toString());
+            /*if(data.getInt("cod") != 200)// != Réussite
+                return null;*/
+            return data;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static JSONObject likeAd(Context context, String _id){
+        try{
+            String urlString = AD_API + "/ads/like/%s";
+            //_id = URLEncoder.encode(_id,"utf-8");
+            urlString = String.format(urlString, _id);
+            URL url = new URL(urlString); //Ajout de l'id dans l'url
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer json = new StringBuffer(1024);
+            String tmp;
+            while((tmp=reader.readLine())!= null)
+                json.append(tmp).append("\n");
+            reader.close();
+            JSONObject data = new JSONObject(json.toString());
+            /*if(data.getInt("cod") != 200)// != Réussite
+                return null;*/
+            return data;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static JSONObject dislikeAd(Context context, String _id){
+        try{
+            String urlString = AD_API + "/ads/dislike/%s";
+            //_id = URLEncoder.encode(_id,"utf-8");
+            urlString = String.format(urlString, _id);
+            URL url = new URL(urlString); //Ajout de l'id dans l'url
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer json = new StringBuffer(1024);
+            String tmp;
+            while((tmp=reader.readLine())!= null)
+                json.append(tmp).append("\n");
+            reader.close();
+            JSONObject data = new JSONObject(json.toString());
+            /*if(data.getInt("cod") != 200)// != Réussite
+                return null;*/
             return data;
         } catch (MalformedURLException e) {
             e.printStackTrace();
