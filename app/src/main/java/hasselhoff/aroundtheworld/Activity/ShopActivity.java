0e708ed127a47007c48742792c5cbec52369ce1c
Licinius <florent.dellomo@gmail.com>
2018-05-23 00:16:51 +0200

    Les trucs de Loïc

--- app/src/main/java/hasselhoff/aroundtheworld/Activity/ShopActivity.java
+++ app/src/main/java/hasselhoff/aroundtheworld/Activity/ShopActivity.java
@@ -1,10 +1,20 @@
 package hasselhoff.aroundtheworld.Activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 import hasselhoff.aroundtheworld.R;
 
@@ -15,10 +25,31 @@ public class ShopActivity extends SubActivity implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(ShopActivity.this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
+        LocationManager locMan = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+            Location location = locMan.getLastKnownLocation(locMan.getBestProvider(criteria, false));
+            if (location != null) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+
+                CameraPosition camPosition = new CameraPosition.Builder()
+                        .target(latLng)      // Sets the center of the map to location user
+                        .zoom(17)                   // Sets the zoom
+                        .bearing(0)                // Sets the orientation of the camera to north
+                        .build();                   // Creates a CameraPosition from the builder
+
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(camPosition));
+            }
+        }
     }
 }
